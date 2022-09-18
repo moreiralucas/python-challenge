12094d70a252f8559e670ef6100362e473a49255
Lucas Moreira <lucas-mopi@htomail.com>
2022-09-18 16:39:35 -0300

    Implements Websites Tests

---
+++ users/tests/test_website.py
@@ -0,0 +1,27 @@
+"""Test User Module"""
+from typing import Dict
+from users.tests.base import BaseTest
+from rest_framework import status
+
+
+class WebsiteTest(BaseTest):
+    """Website Test class"""
+
+    def test_list_websites(self):
+        """Should test list websites"""
+
+        url: str = "/users/websites/"
+        response = self.client.get(path=url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_json: Dict = response.json()
+        self.assertTrue("websites" in response_json)
+        self.assertIsInstance(response_json["websites"], list)
+        self.assertEqual(response_json["websites"][0]["website"], "hildegard.org")
+
+
+    def test_try_list_websites(self):
+        """Should test that unauthenticated user cannot list websites"""
+
+        url: str = "/users/websites/"
+        response = self.client_unauthenticated.get(path=url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
