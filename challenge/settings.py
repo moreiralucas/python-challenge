7e2988809924c9ee846a7cdba1fb821813b874a4
Lucas Moreira <lucas-mopi@htomail.com>
2022-09-18 21:28:45 -0300

    Implements authentication

--- challenge/settings.py
+++ challenge/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,10 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 """Urls for user app"""
 from django.urls import include, path
+from rest_framework.authtoken import views
 from rest_framework import routers
 from .views import UserDetailViewSet, UserSearchViewSet, WebsiteViewset
 
@@ -10,5 +11,5 @@ router_api.register(r'', UserSearchViewSet)
 
 urlpatterns = [
     path("users/", include(router_api.urls)),
-    path('auth/', include('rest_framework.urls'))
+    path('auth/', views.obtain_auth_token)
 ]
